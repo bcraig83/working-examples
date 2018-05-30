9499757dcb6133ea4f17b7fef654cd3e407ffe7f
Ben Craig <bcraig83@gmail.com>
2018-05-30 19:24:24 +0100

    Add simple Spring config file

---
+++ java/spring/spring-rest/src/main/java/b3ls/springrest/WebConfig.java
@@ -0,0 +1,10 @@
+package b3ls.springrest;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig {
+
+}
